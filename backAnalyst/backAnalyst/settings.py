8ea992fc650e15ddeacefa8487b8ebbcb6138d69
miguelgonrod <miguelgonrod2004@gmail.com>
2025-04-20 23:09:32 -0500

    Feat: modified compose

--- backAnalyst/backAnalyst/settings.py
+++ backAnalyst/backAnalyst/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qz!pi$r*jni9x@8&_(z-ip)!@xeaylo5c0q!z77gmo+(_d-se2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 
 
 # Application definition
